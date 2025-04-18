3afb14cdf9adaf393b5d8a2c62c978fb0e9e0caf
Tai Pham <39149250+taipt03@users.noreply.github.com>
2025-04-18 15:29:15 +0700

    Update eval.py only infer

--- eval.py
+++ eval.py
@@ -20,18 +20,15 @@ import cv2
 def _eval(model, args):
     device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
-    state_dict = torch.load(args.test_model,map_location=device)
+    state_dict = torch.load(args.test_model, map_location=device)
     model.load_state_dict(state_dict['model'])
     
     dataloader = test_dataloader(args.data_dir, batch_size=1, num_workers=0)
     torch.cuda.empty_cache()
-    adder = Adder()
     model.eval()
     factor = 8
-    with torch.no_grad():
-        psnr_adder = Adder()
-        ssim_adder = Adder()
 
+    with torch.no_grad():
         for iter_idx, data in enumerate(dataloader):
             input_img, label_img, name = data
 
@@ -43,42 +40,15 @@ def _eval(model, args):
             padw = W-w if w%factor!=0 else 0
             input_img = f.pad(input_img, (0, padw, 0, padh), 'reflect')
 
-            tm = time.time()
-
             pred = model(input_img)[2]
-            pred = pred[:,:,:h,:w]
-
-            elapsed = time.time() - tm
-            adder(elapsed)
+            pred = pred[:, :, :h, :w]
 
             pred_clip = torch.clamp(pred, 0, 1)
 
-            pred_numpy = pred_clip.squeeze(0).cpu().numpy()
-            label_numpy = label_img.squeeze(0).cpu().numpy()
-
-
-            label_img = (label_img).to(device)
-            psnr_val = 10 * torch.log10(1 / f.mse_loss(pred_clip, label_img))
-            down_ratio = max(1, round(min(H, W) / 256))	
-            ssim_val = ssim(f.adaptive_avg_pool2d(pred_clip, (int(H / down_ratio), int(W / down_ratio))), 
-                            f.adaptive_avg_pool2d(label_img, (int(H / down_ratio), int(W / down_ratio))), 
-                            data_range=1, size_average=False)	
-            print('%d iter PSNR_dehazing: %.2f ssim: %f' % (iter_idx + 1, psnr_val, ssim_val))
-            ssim_adder(ssim_val)
-
             if args.save_image:
                 save_name = os.path.join(args.result_dir, name[0])
-                pred_clip += 0.5 / 255
+                pred_clip += 0.5 / 255  # Adjusting for rounding
                 pred = F.to_pil_image(pred_clip.squeeze(0).cpu(), 'RGB')
                 pred.save(save_name)
-            
-            psnr_mimo = peak_signal_noise_ratio(pred_numpy, label_numpy, data_range=1)
-            psnr_adder(psnr_val)
 
-            print('%d iter PSNR: %.2f time: %f' % (iter_idx + 1, psnr_mimo, elapsed))
-
-        print('==========================================================')
-        print('The average PSNR is %.2f dB' % (psnr_adder.average()))
-        print('The average SSIM is %.5f dB' % (ssim_adder.average()))
-
-        print("Average time: %f" % adder.average())
+            print('%d iter Image saved: %s' % (iter_idx + 1, save_name))
